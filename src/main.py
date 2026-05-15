af332b68293be211571d275cfcbc1d03e9465224
ariel-sng <arielsinge@hotmail.com>
2026-05-15 18:34:42 -0300

    Agregado capacidad de leer el prompt del .txt

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from openai import OpenAI
 from dotenv import load_dotenv
+
 from src.models.chat_request import ChatRequest
+from pathlib import Path
 
 # para cargar var. de ambiente, aún nada implementado con OpenAI
 load_dotenv()
@@ -17,3 +19,16 @@ def root():
 @app.post("/chat")
 async def chat(request: ChatRequest):
     return { "recibido": request }
+
+@app.get("/systemprompt")
+def root():
+    main_prompt = getSystemPrompt()
+
+    return { "system_prompt": main_prompt }
+
+def getSystemPrompt():
+    PROMPT_PATH = Path(__file__).parent.parent / "prompt" / "main_prompt.txt"
+
+    with open(PROMPT_PATH, "r", encoding="utf-8") as f:
+        main_prompt = f.read()
+    return main_prompt
